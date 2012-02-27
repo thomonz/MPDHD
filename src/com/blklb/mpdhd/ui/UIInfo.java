d9b2758c42fcb997387e6f5865da9a1c96ee23ef
Andrew Blackburn <almost1570@gmail.com>
2012-02-27 05:11:54 -0500

    added a boolean to indicate ICS vs Honeycomb.

--- src/com/blklb/mpdhd/ui/UIInfo.java
+++ src/com/blklb/mpdhd/ui/UIInfo.java
@@ -3,7 +3,6 @@ package com.blklb.mpdhd.ui;
 import android.graphics.drawable.Drawable;
 
 public class UIInfo {
-	
 	public static Drawable unkownDrawable = null;
-
+	public static boolean isICS = false; 
 }
