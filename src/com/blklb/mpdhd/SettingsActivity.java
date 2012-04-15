7c337f529d92e4749c27bb313cbeb02d3f3e6dc8
Andrew Blackburn <almost1570@gmail.com>
2012-04-15 02:00:01 -0400

    Toying with a new way to hack at the connection bug.

--- src/com/blklb/mpdhd/SettingsActivity.java
+++ src/com/blklb/mpdhd/SettingsActivity.java
@@ -1,18 +1,34 @@
 package com.blklb.mpdhd;
 
+import android.app.Activity;
 import android.os.Bundle;
+
 import android.preference.PreferenceActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-
-import com.blklb.mpdhd.R;
+import com.blklb.mpdhd.tasks.NetworkAndUITask;
+import com.blklb.mpdhd.tools.TimerHelper;
 
 public class SettingsActivity extends PreferenceActivity {
-	
-	//private String tag = "SettingsActivity";
+
+	private String tag = "SettingsActivity";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.addPreferencesFromResource(R.xml.prefrences);		
+		this.addPreferencesFromResource(R.xml.prefrences);
 	}
+
+	// @Override
+	public void selfDestruct(View view) {
+		final Activity a = this.getParent();
+
+		// Reset the connection from start
+		//TimerHelper.getInstance().scheduleTask(new NetworkAndUITask(a), 100);
+		Log.w(tag, "ConnectionReset");
+
+	}
+
 }
